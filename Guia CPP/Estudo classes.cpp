c956be8ac3ff9d9a3e2217fda57d82dfd247c0de
Lucas Santos <lucassantos135b211@gmail.com>
2025-04-28 15:55:00 -0300

    adicionado guia CPP

---
+++ Guia CPP/Estudo classes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+class cadastro{
+	
+	private: 
+		
+		string nome;
+		int idade;
+
+	public:
+		
+		
+	
+};
+
+int main(int argc, char** argv)
+{
+	
+	setlocale(LC_ALL, "");
+	
+		
+	
+	return 0;
+}
